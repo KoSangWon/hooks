5891a5b007d45f4a391da690a58e41c3d4e737da
KoSangWon <jackoss@naver.com>
2020-04-08 02:22:24 +0900

    useInput

---
+++ useInput/index.js
@@ -0,0 +1,15 @@
+export * from './useInput'
+
+
+
+//   사용 예시
+//   const App = () => {
+//     const maxLen = (value) => value.length <= 10;
+//     const name = useInput("Mr.", maxLen);
+//     return (
+//       <div className="App">
+//         <h1>Hello</h1>
+//         <input placeholder="Name" {...name}/>
+//       </div>
+//     );
+//   };
