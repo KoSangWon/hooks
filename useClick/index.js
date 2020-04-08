6320a407cca313196ce9e4f79c4ed699dc110a90
KoSangWon <jackoss@naver.com>
2020-04-09 03:10:17 +0900

    useClick

---
+++ useClick/index.js
@@ -0,0 +1,12 @@
+export * from "./useClick";
+
+//  사용 예시
+// const App = () => {
+//     const sayHello = () => console.log("say hello");
+//     const title = useClick(sayHello);
+//     return (
+//       <div className="App">
+//         <h1 ref={title}>Hi</h1>
+//       </div>
+//     );
+//   };
---
+++ useClick/useClick.js
@@ -0,0 +1,24 @@
+import React, { useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const useClick = (onClick) => {
+  if(typeof onClick !== "function"){
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    //mount 될때, didupdate(deps없을 때), deps가 있다면 그곳에 있는 것이 바뀌었을 때만 실행
+    if(element.current){
+      //reference덕에 동작
+      element.current.addEventListener("click", onClick);//didupdate때마다 호출되면 안되므로 deps = []로 지정
+    }
+    return () => {
+      // function을 리턴한다. componentWillUnMount 때 호출
+      if(element.current){
+        element.current.removeEventListener("click", onClick);
+      }
+    }
+  }, []);
+  
+  return element;
+}
