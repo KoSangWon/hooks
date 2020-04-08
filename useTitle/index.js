0840f5f4b52ec9c391eb75d784a3849e696a6400
KoSangWon <jackoss@naver.com>
2020-04-09 01:28:05 +0900

    useTitle

---
+++ useTitle/index.js
@@ -0,0 +1,12 @@
+export * from "./useTitle";
+
+// 사용 예시
+// const App = () => {
+//     const titleUpdater = useTitle("Loading...");
+//     setTimeout(() => titleUpdater("Home"), 5000);
+//     return (
+//       <div className="App">
+//         <div>Hi</div>
+//       </div>
+//     );
+//   };
