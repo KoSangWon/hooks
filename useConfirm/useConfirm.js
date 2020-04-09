46d7adea8bc734d801197a178616684c9d80b706
KoSangWon <jackoss@naver.com>
2020-04-09 20:17:41 +0900

    useConfirm

---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,16 @@
+export const useConfirm = (message = "", onConfirm, onCancel) => {
+    if(!onConfirm && typeof onConfirm !== "function"){//onConfirm이 존재하는지, 함수인지 체크
+      return;
+    }
+    if(!onCancel && typeof onCancel !== "function"){//onCancel이 존재하는지, 함수인지 체크
+      return;
+    }
+    const confirmAction = () => {
+      if(confirm(message)){
+        onConfirm();
+      }else{
+        onCancel();
+      }
+    }
+    return confirmAction;
+  }
