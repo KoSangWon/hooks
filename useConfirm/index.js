6a3ce1708cc26af27bad4c04798f505b59b9885c
KoSangWon <jackoss@naver.com>
2020-04-09 20:20:56 +0900

    useConfirm

--- useConfirm/index.js
+++ useConfirm/index.js
@@ -1,1 +1,13 @@
 export * from './useConfirm';
+
+// 사용 예시
+// const App = () => {
+//     const deleteWorld = () => console.log("Deleting the world!");
+//     const abort = () => console.log("Aborted")
+//     const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
+//     return (
+//       <div className="App">
+//         <button onClick={confirmDelete}>Delete the world</button>
+//       </div>
+//     );
+//   };
